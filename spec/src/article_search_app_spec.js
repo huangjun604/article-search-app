aee5071c16ad319211b1b33647dbc1ac726d8dd6
Stephen Huang <huangjun604@gmail.com>
2017-10-08 17:24:19 +1100

    Update test code

--- spec/src/article_search_app_spec.js
+++ spec/src/article_search_app_spec.js
@@ -10,15 +10,107 @@ describe('ArticleSearchApp', () => {
     app = new ArticleSearchApp(client, { metadata: {}, context: {} });
   });
 
+  describe('onSearchKeyPressed', () => {
+    beforeEach(() => {
+      spyOn(app, 'searchArticles');
+    });
+
+    it('should search articles', () => {
+      const e = {
+        keyCode: 13,
+        preventDefault: () => null
+      };
+
+      app.onSearchKeyPressed(e);
+
+      expect(app.searchArticles).toHaveBeenCalled();
+    });
+  });
+
+
+  describe('onSearchIconClicked', () => {
+    beforeEach(() => {
+      spyOn(app, 'searchArticles');
+    });
+
+    it('should search articles', () => {
+      const e = {
+        preventDefault: () => null
+      };
+
+      app.onSearchIconClicked(e);
+
+      expect(app.searchArticles).toHaveBeenCalled();
+    });
+  });
+
+  describe('onPageLinkClicked', () => {
+    beforeEach(() => {
+      spyOn(app, 'ajax');
+    });
+
+    it('should fetch articles', () => {
+      const link = document.createElement('a');
+      link.dataset.url = 'http://example.com';
+      const e = {
+        currentTarget: link,
+        preventDefault: () => null
+      };
+
+      app.onPageLinkClicked(e);
+
+      expect(app.ajax).toHaveBeenCalledWith('fetchArticles', 'http://example.com');
+    });
+
+  });
+
   describe('#searchArticles', () => {
     beforeEach(() => {
       spyOn(app, 'ajax').and.returnValue();
+      spyOn(app, 'getSearchQuery').and.returnValue('test');
     });
 
     const query = 'domain';
 
     it('should query the articles', () => {
+      const url = '/api/v2/help_center/articles/search.json?per_page=5&query=test';
 
+      app.searchArticles();
+      expect(app.ajax).toHaveBeenCalledWith("fetchArticles", url);
+    });
+  });
+
+  describe('#loadResults', () => {
+    beforeEach(() => {
+
+    });
+  });
+
+  describe('#onShareButtonClicked', () => {
+    beforeEach(() => {
+      spyOn(app, 'shareToSupport');
+      spyOn(app, 'shareToChat');
+    });
+
+    const shareButton = document.createElement('button');
+    shareButton.dataset.url = 'http://example.com';
+    const e = {
+      currentTarget: shareButton,
+      preventDefault: () => null
+    };
+
+    it('should call share to support', () => {
+      spyOn(app, 'currentLocation').and.returnValue('ticket_sidebar');
+
+      app.onShareButtonClicked(e);
+      expect(app.shareToSupport).toHaveBeenCalledWith('http://example.com');
+    });
+
+    it('should call share to chat', () => {
+      spyOn(app, 'currentLocation').and.returnValue('chat_sidebar');
+
+      app.onShareButtonClicked(e);
+      expect(app.shareToChat).toHaveBeenCalledWith('http://example.com');
     });
   });
 
@@ -26,17 +118,10 @@ describe('ArticleSearchApp', () => {
     beforeEach(() => {
       spyOn(client, 'set');
     });
+    const url = 'http://example.com/';
 
     it('should post the url to support comment box', () => {
-      const shareButton = document.createElement('button');
-      shareButton.dataset.url = 'http://example.com/';
-
-      const e = {
-        currentTarget: shareButton,
-        preventDefault: () => null
-      };
-
-      app.shareToSupport(e);
+      app.shareToSupport(url);
 
       expect(client.set).toHaveBeenCalledWith('comment.text', 'http://example.com/');
     });
@@ -47,16 +132,10 @@ describe('ArticleSearchApp', () => {
       spyOn(client, 'invoke');
     });
 
+    const url = 'http://example.com/';
+
     it('should post the url to chat text area', () => {
-      const shareButton = document.createElement('button');
-      shareButton.dataset.url = 'http://example.com/';
-
-      const e = {
-        currentTarget: shareButton,
-        preventDefault: () => null
-      };
-
-      app.shareToChat(e);
+      app.shareToChat(url);
 
       expect(client.invoke).toHaveBeenCalledWith('chat.postToChatTextArea', 'http://example.com/');
     });
